d800178f356d8db9187b2979e168f30500c4d0c0
Adamheadshot <adam.alsaadi@elev.ga.ntig.se>
2024-05-28 14:10:05 +0200

    Initial commit

---
+++ src/AdamKlient.java
@@ -0,0 +1,37 @@
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+
+
+public class AdamKlient {
+    public static void main(String[] args) {
+        String hostName = "localhost";
+        int portNumber = 4446;
+        try (
+                Socket socket = new Socket(hostName, portNumber);
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                Scanner scanner = new Scanner(System.in)
+        ) {
+            String userInput;
+            System.out.println("\n\nConnected to the server. Write in this format '1 + 1");
+            while (true) {
+                System.out.print("Enter quit to escape");
+                userInput = scanner.nextLine();
+
+                if ("quit".equalsIgnoreCase(userInput)) {
+                    break;
+                }
+                out.println(userInput);
+                String response = in.readLine();
+                System.out.println("Server response: " + response);
+            }
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " + hostName);
+            System.exit(1);
+        }
+    }
+}
